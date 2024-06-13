4f7792b6276ab9a261ea09a6ae9f1a1a359d11e7
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-06-13 09:16:30 +0530

    Add files via upload

---
+++ tuple8.py
@@ -0,0 +1,15 @@
+count=0
+s=0
+tuple=(12,3,45,66,7,3,44,65,49,56)
+for i in tuple:
+    if i%7==0:
+        print(i)
+        s=s+i
+
+
+        count+=1
+print(s,"sum")
+print(count,"=count")
+"""
+Find multiple of 7 and sum 
+"""
