bb313cef5bbc9e58fdf8a39a4159767b423bd594
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-06-26 15:59:22 +0530

    Add files via upload

---
+++ p8SubjecTotalAvg.py
@@ -0,0 +1,7 @@
+maths=89
+science=90
+english=98
+hindi=78
+account=78
+print("total of all subjects =",maths+english+science+hindi+account)
+print("average of all subjects",(maths+english+science+hindi+account)/5)
