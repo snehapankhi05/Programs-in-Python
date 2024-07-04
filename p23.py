04e47ec8f85cec2fbd7fcdc88a1e04f9d5162c42
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-07-04 14:32:34 +0530

    Add files via upload

---
+++ p23.py
@@ -0,0 +1,6 @@
+a=int(input("please enter your first number= "))
+b=int(input("please enter your second number= "))
+if a>b:
+    print("The larger no is=",a)
+else:
+    print("the larger no is =",b)
