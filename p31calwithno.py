186c161d41d81f0df13b9c4e54110c8329c47608
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-07-10 12:43:36 +0530

    Add files via upload

---
+++ p31calwithno.py
@@ -0,0 +1,19 @@
+print("press 1 for additon ")
+print("press 2 for subtraction ")
+print("press 3 for multiplication ")
+print("press 4 for division ")
+a=int(input("enter your option= "))
+b=int(input("enter your 1st number="))
+c=int(input("enter your second number"))
+if a==1:
+    print(b+c)
+elif a==2:
+    print(b-c)
+elif a==3:
+    print(b*c)
+elif a==4:
+    print(b*c)
+else:
+    print("You have selected wrong option ")
+
+a=int(input(""))
