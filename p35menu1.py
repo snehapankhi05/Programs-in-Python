2b2847d1554e64c53721a0512d311657884190d9
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-07-13 17:44:38 +0530

    Add files via upload

---
+++ p35menu1.py
@@ -0,0 +1,18 @@
+dayno=int(input("Enter dayno =>"))
+
+if dayno==1:
+    print("monday")
+elif dayno==2:
+    print("tuesday")
+elif dayno==3:
+    print("wednesday")
+elif dayno == 4:
+        print("thrusday")
+elif dayno == 5:
+        print("friday")
+elif dayno == 6:
+        print("saturday")
+elif dayno==7:
+    print("sunday")
+else:
+    print("you have selected wrong no")
