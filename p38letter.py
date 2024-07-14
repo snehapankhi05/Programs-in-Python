159260023b1b9fe600ce884a7b6ff172dcf40812
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-07-14 17:00:21 +0530

    Add files via upload

---
+++ p38letter.py
@@ -0,0 +1,14 @@
+ch=input("Enter character =>")
+
+if ch=="a" or ch=="A":
+    print("its vowel")
+elif ch=="e" or ch=="E":
+    print("its vowel")
+elif ch=="i" or ch=="I":
+    print("its vowel")
+elif ch=="o" or ch=="O":
+    print("its vowel")
+elif ch=="u" or ch=="U":
+    print("its vowel")
+else:
+    print("its a consonat")
