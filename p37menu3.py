627817ea06bfbd74722c0f96a4a38628cea83cca
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-07-14 16:59:39 +0530

    Add files via upload

---
+++ p37menu3.py
@@ -0,0 +1,11 @@
+print("Press 1 for square")
+print("Press 2 for cube")
+op=int(input("Enter option =>"))
+a=int(input("give  number"))
+
+if op==1:
+    print("the square of number is ",a*a)
+elif  op==2:
+        print("the cube of number is",a*a*a)
+else:
+    print("u have selected wrong option")
