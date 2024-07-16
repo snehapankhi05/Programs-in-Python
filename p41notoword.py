14c21c010631c1a907b0f0e127a6d134a773c7ac
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-07-16 17:43:52 +0530

    Add files via upload

---
+++ p41notoword.py
@@ -0,0 +1,22 @@
+a=int(input("enter the number from 1 to 9 "))
+if a==1:
+    print("one")
+elif a==2:
+    print("two")
+elif a==3:
+    print("three")
+elif a==4:
+    print("four")
+elif a==5:
+    print("five")
+elif a==6:
+    print("six")
+elif a==7:
+    print("seven")
+elif a==8:
+    print("eight")
+elif a==9:
+    print("nine")
+else:
+    print("select number between 1 to 9")
+
