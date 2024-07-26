91bcba4b3c7ebdcdfd8e5618badcf925fa5c646f
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-07-26 18:05:49 +0530

    Add files via upload

---
+++ p61loop.py
@@ -0,0 +1,15 @@
+limit = int(input("Enter the limit for the series: "))
+sum_series = 0
+series = " "
+
+for i in range(1, limit+1):
+    term = (-1)**(i+1) * i**2
+    sum_series += term
+    series += str(term) + " "
+
+print("Series:", series)
+print("Sum:", sum_series)
+
+"""
+1-4+9-16+25 ...sum
+"""
