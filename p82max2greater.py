1d8f4b866f94fa0358fa12a378050f5355d98629
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-08-10 18:24:56 +0530

    Add files via upload

---
+++ p82max2greater.py
@@ -0,0 +1,9 @@
+def max():
+    n=int((input("enter 1st number")))
+    m=int(input("enter 2nd number"))
+    if n>m:
+        print("larger is==",n)
+    else:
+        print("larger is",m)
+max()
+        
