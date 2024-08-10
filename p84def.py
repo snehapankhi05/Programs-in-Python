acec30c71473d8e1fd14949db250dba5a2c1e3a4
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-08-10 18:25:36 +0530

    Add files via upload

---
+++ p84def.py
@@ -0,0 +1,16 @@
+x=float(input("enter your first value"))
+y=float(input("enter your second number"))
+print("addition=",x+y)
+print("div=",x/y)
+print("sub=",x-y)
+print("multiplication=",x*y)
+
+if x%2==0:
+    print("its even")
+else:
+    print(" its odd")
+
+    if x>y:
+        print("larger is==",x)
+    else:
+        print("larger is",y)
