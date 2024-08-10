63588218141c151c5c289c1ef1a637fd8e552a02
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-08-10 18:24:34 +0530

    Add files via upload

---
+++ p81oddeven.py
@@ -0,0 +1,10 @@
+def oddeven():
+    n = int(input("enter number"))
+    i = 1
+    while i <= n:
+        if i % 2 == 0:
+            print(i, "is even")
+        else:
+            print(i, "is odd")
+        i += 1
+oddeven()
